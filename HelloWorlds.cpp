308f9755a6b8a976a816c7c1a178b6485befa4bb
Hill <khill4s@semo.edu>
2018-01-29 19:07:23 -0600

    Fixed merge

--- HelloWorlds.cpp
+++ HelloWorlds.cpp
@@ -1,31 +1,21 @@
 #include <iostream> 
 
-<<<<<<< HEAD
 int multiply(int x, int y);
-
-int main() {
-=======
 int add(int x, int y); 
 
 int main() 
 {
->>>>>>> 3bc52daec50e89546ee18d50728ce892e7d3ce31
-
 	std::cout << "Hello World!\n";
 
 	return 0;
-<<<<<<< HEAD
 }
 
 int multiply(int x, int y)
 {
 	return x * y;
 }
-=======
-}	
 
 int add(int x, int y);
 {
 	return x + y;
 }
->>>>>>> 3bc52daec50e89546ee18d50728ce892e7d3ce31
